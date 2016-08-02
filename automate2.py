a06df8db75a89b163fe5d5a1c0e732113e95a50a
assambapasquier <assambapasquier@gmail.com>
2016-08-02 18:44:07 +0200

    Media link Scraper
    This automate that can scrap all media link from websites giving a list
    of urls.

---
+++ automate2.py
@@ -0,0 +1,72 @@
+import urllib.request as ur
+import re
+#this class has to pull all link conatained in a web page
+class Automate:
+	ur = __import__('urllib.request')
+	re = __import__('re')
+	def __init__(self, url_list): #txt file containing urls to open
+		self.url_list = url_list
+		self.link = ""
+		self.pathlink = ""
+		
+		#opening files
+		f = open('web_sourcecode.txt', 'w+');
+		f2 = open('media_links.csv', 'w+');
+		print('web site;media link', file=f2);
+		#fi = open(self.url_list, 'a+');
+		#header of the csv file
+		#fi.seek(0,0);
+		with open(self.url_list) as fi:
+			lines_list = fi.read().splitlines(); #making a list (array of links)
+		
+		for links in lines_list:#retrieving every links in the list
+			print(links);
+			try: 
+				req = ur.Request(links.rstrip(), headers={'User-Agent': "Mozilla/5.0' (X11; Windows x86_64) Chrome/12.0.742.112"});
+				self.s = ur.urlopen(req);
+				self.sl = self.s.read();
+				print(self.sl, file=f);
+				f.seek(0,0);#restart to the beginning
+				self.lines = f.read();
+				if self.lines.find('<a href="') != -1:
+					self.expression = r"<a href=\"";
+					array = [m.start() for m in re.finditer(self.expression, self.lines)]
+					#print(array);
+					for l in range(0, len(array)):
+						j = array[l];
+						while (self.lines[j+9] != '"'):
+							self.link += self.lines[j+9];
+							j = j+1;
+						if self.link.lower().find('/media')!=-1 or self.link.lower().find('/multimedia')!=-1 or self.link.lower().find('/tv')!=-1 or self.link.lower().find('/movie')!=-1 or self.link.lower().find('/categories')!=-1 or self.link.lower().find('/shows') !=-1 or self.link.lower().find('/video')!=-1 or self.link.lower().find('/film')!=-1 or self.link.lower().find('_tv')!=-1 or self.link.lower().find('news')!=-1 or self.link.lower().find('tv_')!=-1 or self.link.lower().find('live')!=-1 or self.link.lower().find('radio')!=-1 or self.link.lower().find('/playlist')!=-1 or self.link.lower().find('/game')!=-1or self.link.lower().find('podcast')!=-1 or self.link.lower().find('webinar')!=-1 or self.link.lower().find('animation')!=-1 or self.link.lower().find('documentarie')!=-1 or self.link.lower().find('/caf')!=-1:
+							if self.link.find('http://') == -1:
+								self.link = links.rstrip() + self.link;
+							if self.link.rstrip() != '#':
+								exists = f2.readlines();
+								found = False;
+								for li in exists:
+									if self.link.rstrip().replace("//", "/") in li:
+										found = True;
+										break;
+								if not found: #does not already exist
+									print(links.rstrip() +';' + self.link.rstrip().replace("//", "/"), file=f2)
+						if self.link.rstrip()!='#':
+							#print(self.link.rstrip().replace("//", "/"), file=fi)
+							if self.link.rstrip().endswith('/'): #xxxxx/ is the same thing as xxxx. so we remove the last / in an url.
+								self.link = self.link.rstrip()[:-1];
+							if self.link not in lines_list: #adding a link to the list if not exist
+								lines_list.append(self.link.rstrip());
+						self.link = ""
+				else: #not href found= not link found in that page
+					print ('false: not link found in that page ');
+			except (ur.HTTPError, ValueError, ur.URLError) as e:
+				pass
+			
+		f.close();
+		#fi.close();
+		f2.close();
+		
+def main():
+	aut = Automate('list_url2.txt');
+	
+if  __name__ =='__main__':
+	main()
