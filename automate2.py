e885e4cbfc209b418ee35f70bf0c618b4f96c5d5
assambapasquier <assambapasquier@gmail.com>
2016-08-03 17:54:50 +0200

    Scraper

--- automate2.py
+++ automate2.py
@@ -10,14 +10,16 @@ class Automate:
 		self.pathlink = ""
 		
 		#opening files
-		f = open('web_sourcecode.txt', 'w+');
-		f2 = open('media_links.csv', 'w+');
+		f = open('C:/Users/pasquierase/Desktop/Projet UpWork AI/SIRI/web_sourcecode.txt', 'w+');
+		f2 = open('C:/Users/pasquierase/Desktop/Projet UpWork AI/SIRI/media_links.csv', 'w+');
 		print('web site;media link', file=f2);
 		#fi = open(self.url_list, 'a+');
 		#header of the csv file
 		#fi.seek(0,0);
-		with open(self.url_list) as fi:
-			lines_list = fi.read().splitlines(); #making a list (array of links)
+		#with open(self.url_list) as fi:
+		#	lines_list = fi.read().splitlines(); #making a list (array of links)
+			
+		lines_list = list([self.url_list]); #making a list (array of links)
 		
 		for links in lines_list:#retrieving every links in the list
 			print(links);
@@ -37,24 +39,18 @@ class Automate:
 						while (self.lines[j+9] != '"'):
 							self.link += self.lines[j+9];
 							j = j+1;
-						if self.link.lower().find('/media')!=-1 or self.link.lower().find('/multimedia')!=-1 or self.link.lower().find('/tv')!=-1 or self.link.lower().find('/movie')!=-1 or self.link.lower().find('/categories')!=-1 or self.link.lower().find('/shows') !=-1 or self.link.lower().find('/video')!=-1 or self.link.lower().find('/film')!=-1 or self.link.lower().find('_tv')!=-1 or self.link.lower().find('news')!=-1 or self.link.lower().find('tv_')!=-1 or self.link.lower().find('live')!=-1 or self.link.lower().find('radio')!=-1 or self.link.lower().find('/playlist')!=-1 or self.link.lower().find('/game')!=-1or self.link.lower().find('podcast')!=-1 or self.link.lower().find('webinar')!=-1 or self.link.lower().find('animation')!=-1 or self.link.lower().find('documentarie')!=-1 or self.link.lower().find('/caf')!=-1:
+						if self.link.lower().find('/media')!=-1 or self.link.lower().find('/multimedia')!=-1 or self.link.lower().find('/tv')!=-1 or self.link.lower().find('/movie')!=-1 or self.link.lower().find('/categories')!=-1 or self.link.lower().find('/shows') !=-1 or self.link.lower().find('/video')!=-1 or self.link.lower().find('/film')!=-1 or self.link.lower().find('_tv')!=-1 or self.link.lower().find('news')!=-1 or self.link.lower().find('tv_')!=-1 or self.link.lower().find('live')!=-1 or self.link.lower().find('_live')!=-1 or self.link.lower().find('live_')!=-1 or self.link.lower().find('radio')!=-1 or self.link.lower().find('/playlist')!=-1 or self.link.lower().find('/game')!=-1or self.link.lower().find('podcast')!=-1 or self.link.lower().find('webinar')!=-1 or self.link.lower().find('animation')!=-1 or self.link.lower().find('documentarie')!=-1 or self.link.lower().find('/caf')!=-1:
 							if self.link.find('http://') == -1:
-								self.link = links.rstrip() + self.link;
-							if self.link.rstrip() != '#':
-								exists = f2.readlines();
-								found = False;
-								for li in exists:
-									if self.link.rstrip().replace("//", "/") in li:
-										found = True;
-										break;
-								if not found: #does not already exist
-									print(links.rstrip() +';' + self.link.rstrip().replace("//", "/"), file=f2)
+								self.link = self.url_list + self.link;
+							if self.link.find('../')!=-1:
+								self.link = self.link.replace('../', '/');
 						if self.link.rstrip()!='#':
 							#print(self.link.rstrip().replace("//", "/"), file=fi)
 							if self.link.rstrip().endswith('/'): #xxxxx/ is the same thing as xxxx. so we remove the last / in an url.
 								self.link = self.link.rstrip()[:-1];
-							if self.link not in lines_list: #adding a link to the list if not exist
+							if self.link.rstrip() not in lines_list: #adding a link to the list if not exist
 								lines_list.append(self.link.rstrip());
+								print(self.url_list +';' + self.link.rstrip(), file=f2)
 						self.link = ""
 				else: #not href found= not link found in that page
 					print ('false: not link found in that page ');
@@ -66,7 +62,7 @@ class Automate:
 		f2.close();
 		
 def main():
-	aut = Automate('list_url2.txt');
+	aut = Automate('http://www.wwitv.com');
 	
 if  __name__ =='__main__':
 	main()
